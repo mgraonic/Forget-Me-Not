321eb62d905e1c01be2cc1c77681ea0091435313
Maja Graonic <m.graonic@gmail.com>
2018-07-09 10:39:50 -0700

    added function to show snapshot  of values in firebase on component mount

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import './App.css';
 import firebase from './firebase.js';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentLevel: '',
+      items: []
+    }
+  }
+  // grabs snapshot of DB on load
+  // pulls levels out
+  componentDidMount() {
+    const levelsRef = firebase.database().ref('levels');
+    levelsRef.on('value', (snapshot) => {
+      let levels = snapshot.val();
+      let newState = [];
+      for (let level in levels) {
+        newState.push({
+          id: level,
+          title: levels[level].title,
+        });
+      }
+      this.setState({
+        levels: newState
+      });
+    });
+  }
   render() {
     return (
       <div className="App">
